8f81264040a60f4cab8a53636673c1cbda51f545
Yusf4 <youssefmazloum2017@gmail.com>
2025-01-17 22:05:21 +0200

    start matplot library

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot  as plt
 
 arr=np.array([1,2,3])
 matrix=np.array([[1,2],[3,4]])
@@ -19,7 +20,13 @@ data={'Name':['Alice','Bob'],'Age':[25,39]}
 df=pd.DataFrame(data)
 print(f"name:{df['Name']}")
 print(f"age:{df.iloc[0]}")
+
+print(f"filtered Age:{df[df['Age']>30]}")
+print(f"mean Age:{df['Age'].mean()}")
 # Press the green button in the gutter to run the script.
+plt.plot([1,2,3],[4,5,6])
+plt.show()
+#plt.scatter([1,2,3],[4,5,6])
 if __name__ == '__main__':
     print('PyCharm')
 
